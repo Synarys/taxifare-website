dad3392f3dbbebbec3d3cb43aae19e9c9796046d
baptiste cellier <baptiste.cellier@outlook.com>
2025-06-06 12:06:38 +0200

    Second website

--- app.py
+++ app.py
@@ -1,41 +1,47 @@
 import streamlit as st
+import pandas as pd
 from datetime import datetime
 import requests
 
-'''
-# TaxiFareModel front
-'''
+
+'# 🚕 TaxiFareModel'
+
 
 # Initialisation une seule fois
 if "init_now" not in st.session_state:
     st.session_state.init_now = datetime.now()
 
+'### ⏰ When ?'
 # Date
-st.markdown('### When ?')
 date = st.date_input('When do you need a ride ?',st.session_state.init_now.date(), min_value=st.session_state.init_now.date())
 
 # Time
 time = st.time_input('What time is your ride ?', st.session_state.init_now.time())
 
-# Pickup longitude
-st.markdown('### Where ?')
-pickup_longitude = st.number_input('Insert the longitude where you need to be pickup', format='%.6f', step=.000001)
-
-# Pickup latitude
-pickup_latitude = st.number_input('Insert the latitude where you need to be pickup', format='%.6f', step=.000001)
+'### 📍 Where ?'
+col1, col2 = st.columns(2)
 
 # Pickup longitude
-dropoff_longitude = st.number_input('Insert the longitude where you need to be dropoff', format='%.6f', step=.000001)
+with col1:
+    pickup_longitude = st.number_input('Insert the longitude where you need to be pickup', format='%.6f', min_value=-74.02, max_value=-73.92, step=.000001)
 
 # Pickup latitude
-dropoff_latitude = st.number_input('Insert the latitude where you need to be dropoff', format='%.6f', step=.000001)
+with col2:
+    pickup_latitude = st.number_input('Insert the latitude where you need to be pickup', format='%.6f', min_value=40.70, max_value=40.88,step=.000001)
 
+# Pickup longitude
+with col1:
+    dropoff_longitude = st.number_input('Insert the longitude where you need to be dropoff', format='%.6f', min_value=-74.02, max_value=-73.92, step=.000001)
+
+# Pickup latitude
+with col2:
+    dropoff_latitude = st.number_input('Insert the latitude where you need to be dropoff', format='%.6f', min_value=40.70, max_value=40.88,step=.000001)
+
+'### 🧍 How many ?'
 # Passenge Count
-st.markdown('### How many ?')
-passenger_count = st.number_input('How many will there be?', format='%d', min_value=1, value=1, step=1)
+passenger_count = st.number_input('How many will there be?', format='%d', min_value=1, max_value=4, value=1, step=1)
 
 # API call
-
 url = 'https://taxifare.lewagon.ai/predict'
 params ={
     "pickup_datetime": datetime.combine(date, time),
@@ -53,3 +59,7 @@ if st.button('Estimate fare'):
         st.write("Estimated fare :", f'${round(result['fare'], 2)}')
     else:
         st.error(f"Erreur {response.status_code}")
+
+'### 🗺️ Your travel :'
+map_data = pd.DataFrame({'lat': [pickup_latitude, dropoff_latitude], 'lon': [pickup_longitude, dropoff_longitude]})
+st.map(map_data)
